06e4fd1f53277e4770d53651ec809496ed0bb16d
satoshi.morimatsu <morimatu@mac.com>
2022-03-27 04:40:29 +0000

    2022/03/27 13:38:39 users#create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-
+  
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, only: [:index, :show, :new, :create]
+  
+  #resources :tasks, only: [:create, :destroy]
   resources :tasks
+  #  2022/03/27 11:18:29
 end
 
